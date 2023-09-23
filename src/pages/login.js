4e6b14a2a8fb7e6e89aa0611451ed13ca086182a
sakshi <sakshikhapekar123@example.com>
2023-09-23 18:25:49 +0530

    third commit

--- src/pages/login.js
+++ src/pages/login.js
@@ -37,9 +37,10 @@ function Login(props) {
     }
     return true;
    }
-const handleLogin = (event) => {
+  const handleLogin = (event) => {
 
- event.preventDefault(); 
+    
+    event.preventDefault(); 
   
     
 
@@ -70,24 +71,10 @@ const handleLogin = (event) => {
     
 
   };
-  const handleGoogleLogin = () => {
-    // Redirect the user to the Google OAuth authorization URL
-    window.location.href = ' https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&response_type=code&client_id=47060963969-tkec9em931dv0gohp4ti485fa8coatpj.apps.googleusercontent.com%20&redirect_uri=https%3A%2F%2Fblogappfrontend-iawn.onrender';
-  };
+  const handleGoogleLogin=()=>{
+    window.location.href="https://blog-zozd.onrender.com";
+  }
 
-  const handleGitHubLogin = () => {
-    // Redirect the user to the GitHub OAuth authorization URL
-    fetch('https://blog-zozd.onrender.com/github-auth') // Update the URL as needed to match your server route
-      .then((response) => response.json())
-      .then((data) => {
-        // Redirect the user to the GitHub OAuth authorization URL
-        window.location.href = data.githubAuthUrl; // Assuming your server returns the URL as 'githubAuthUrl'
-      })
-      .catch((error) => {
-        console.error('Error fetching GitHub authorization URL:', error);
-      });
-    //window.location.href = 'URL_TO_GITHUB_OAUTH_AUTHORIZATION';
-  };
 
   return (
     <div className="login-container">
@@ -117,12 +104,13 @@ const handleLogin = (event) => {
       </form>
   <div className="login-error-message">{loginError}</div>
   <div className="oauth-buttons">
-        <button className="login-button" onClick={handleGoogleLogin}>
+        {/* <button className="login-button" onClick={handleGoogleLogin}>
           Continue with Google
         </button>
         <button className="login-button" onClick={handleGitHubLogin}>
           Continue with GitHub
-        </button>
+        </button> */}
+        <button onClick={handleGoogleLogin}>Login in with Google</button>
       </div>
     </div>
   );
